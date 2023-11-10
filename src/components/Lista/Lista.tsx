789d761a5730c977712aac9b044cd065dd1fa90d
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-11-10 11:07:10 -0300

    mostrando tarefa selecionada

--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,15 +1,20 @@
 import Item from "./Item/Item"
 import style from "./Lista.module.scss"
 import { Itarefa } from "../../types/tarefa"
-function Lista({  tarefas  } : {tarefas: Itarefa[]}) {
+interface Props{
+    tarefas: Itarefa[]
+    selecionaTarefa: (tarefaSelecionada: Itarefa) => void
+  }
+function Lista({  tarefas, selecionaTarefa  } : Props) {
     return (
         <aside className={style.listaTerefas}>
             <h2>Estudos do dia</h2>
             <ul >
-                {tarefas.map((item, index) => {
+                {tarefas.map((item) => {
                     return (
                         <Item
-                            key={index}
+                            key={item.id}
+                            selecionaTarefa = {selecionaTarefa}
                             // tarefa={item.tarefa}
                             // tempo={item.tempo}
                             {...item}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,13 +4,25 @@ import Lista from "../components/Lista/Lista"
 import { Itarefa } from "../types/tarefa"
 import style from "./App.module.scss"
 import {useState} from "react"
+
+
 function App() {
   const [tarefas, setTarefas] = useState<Itarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<Itarefa>()
+  const selecionaTarefa = (tarefaSelecionada : Itarefa) =>{
+    setSelecionado(tarefaSelecionada)
+    setTarefas(tarefasAnteriosres => tarefasAnteriosres.map(tarefa =>({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
   return (
     <>
       <div className={style.AppStyle}>
         <Form setTarefas={setTarefas}/>
-        <Lista tarefas={tarefas}/>
+        <Lista 
+          tarefas={tarefas}
+          selecionaTarefa = {selecionaTarefa}/>
         <Cronometro/>
       </div>
     </>
